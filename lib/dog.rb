884cfc6926708c426dde5fb6990d3bac7185647d
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 17:59:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
-  attr_accessor :name
-  
-  @@all=[]
-  
+attr_accessor :name, :dog, :all
+@@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
   def self.all
-    @@all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
   end
 end
